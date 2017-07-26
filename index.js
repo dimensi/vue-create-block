f72e809aee56c95c5a4c0c026c7fa3a8e1aff327
Nikita Nafranets <eddimensi@gmail.com>
2017-07-26 15:56:39 +0300

    improve, add change for functions

--- index.js
+++ index.js
@@ -17,39 +17,50 @@ const folders = {
   element: path.join(__dirname, 'src/elements')
 };
 
+const baseFunctions = {
+  props: 'const props = [];',
+  data: `const data = function () {
+  return {
+
+  };
+};`,
+  computed: 'const computed = {};',
+  methods: 'const methods = {};',
+  created: 'const created = function () {};',
+  mounted: 'const mounted = function () {};',
+  components: 'const components = {};'
+};
+
+const generateFunctions = (arrFunc) => {
+  if (Array.isArray(arrFunc)) {
+    return arrFunc.map(name => baseFunctions[name]).join('\n\n');
+  }
+  if (arrFunc === 'all') {
+    return Object.keys(baseFunctions).map(name => baseFunctions[name]).join('\n\n');
+  }
+};
+
+const generateExports = (arrFunc) => {
+  if (Array.isArray(arrFunc)) {
+    return arrFunc.join(',\n  ');
+  }
+  if (arrFunc === 'all') {
+    return Object.keys(baseFunctions).map(name => name).join(',\n  ');
+  }
+};
+
+
 const fileSources = {
-  vue: blockName => `<template>
+  vue: (blockName, arrFunc) => `<template>
   <div class="${changeCase.paramCase(blockName)}"></div>
 </template>
 
 <script>
-const props = [];
-
-const data = function () {
-  return {
-
-  };
-};
-
-const computed = {};
-
-const methods = {};
-
-const created = function () {};
-
-const mounted = function () {};
-
-const components = {};
+${generateFunctions(arrFunc)}
 
 export default {
   name: '${changeCase.paramCase(blockName)}',
-  props,
-  data,
-  computed,
-  methods,
-  created,
-  mounted,
-  components
+  ${generateExports(arrFunc)}
 };
 </script>
 
@@ -71,8 +82,8 @@ class makeBlock {
 
   async directoryOrFileExist(blockPath, blockName) {
     try {
-      const access = await fsAccess(blockPath, fs.constants.W_OK);
-      throw `Error >>> Данный файл или директория уже существует ${blockName || blockPath}`
+      await fsAccess(blockPath, fs.constants.W_OK);
+      throw `Error >>> Данный файл или директория уже существует ${blockName || blockPath}`;
     } catch (err) {
       if (err instanceof Error && err.code === 'ENOENT') {
         return true;
@@ -83,29 +94,30 @@ class makeBlock {
 
   async createDir(dirPath) {
     try {
-      await fsMkDir(dirPath)
+      await fsMkDir(dirPath);
       return true;
     } catch (err) {
-      throw `Error >>> Данная папка уже существует ${dirPath}`
+      throw `Error >>> Данная папка уже существует ${dirPath}`;
     }
   }
 
   async createFiles(blockPath, blockName) {
     const files = [];
+    const functions = this.options.defaultFunctions ? 'all' : this.options.functions;
 
 
     for (let ext in fileSources) {
-      const fileSource = fileSources[ext](blockName);
+      const fileSource = fileSources[ext](blockName, functions);
       const filename = `${blockName}.${ext}`;
       const filePath = path.join(blockPath, filename);
 
       try {
         await this.directoryOrFileExist(filePath);
       } catch (err) {
-        throw `Error >>> Данный файл уже существует ${filePath}`
+        throw `Error >>> Данный файл уже существует ${filePath}`;
       }
       
-      files.push(fsWriteFile(filePath, fileSource, 'utf-8'))
+      files.push(fsWriteFile(filePath, fileSource, 'utf-8'));
     }
 
     return await Promise.all(files);
@@ -113,18 +125,18 @@ class makeBlock {
 
   async getFiles (blockPath, blockName) {
     try {
-      const files = await fsReadDir(blockPath)
+      const files = await fsReadDir(blockPath);
       const file = files.filter(file => file.includes(blockName));
 
-      console.log('\n')
-      console.log(`Блок ${blockName} создан`)
+      console.log('\n');
+      console.log(`Блок ${blockName} создан`);
       console.log('-'.repeat(48));
       file.forEach(name => {
         console.log(name);
-      })
+      });
       console.log('#'.repeat(48));
 
-      return true
+      return true;
     } catch (err) {
       console.error(err);
       throw err;
@@ -132,17 +144,17 @@ class makeBlock {
   }
 
   async makeBlock(blockName) {
-    let blockPath = path.join(folders[this.options.typeBlock])
+    let blockPath = path.join(folders[this.options.typeBlock]);
     if (this.options.createWithDir) {
-      blockPath = path.join(folders[this.options.typeBlock], blockName)
+      blockPath = path.join(folders[this.options.typeBlock], blockName);
     }
 
     if (this.options.createWithDir) {
-      await this.directoryOrFileExist(blockPath)
-      await this.createDir(blockPath)
+      await this.directoryOrFileExist(blockPath);
+      await this.createDir(blockPath);
     }
     try {
-      await this.createFiles(blockPath, blockName)
+      await this.createFiles(blockPath, blockName);
     } catch (err) {
       console.log(err);
       return false;
@@ -153,14 +165,14 @@ class makeBlock {
   }
 
   create() {
-    const blocks = this.options.blockName
+    const blocks = this.options.blockName;
 
     if (Array.isArray(blocks)) {
       const files = blocks.map(name => this.makeBlock(name));
       return Promise.all(files);
     }
 
-    return this.makeBlock(blocks)
+    return this.makeBlock(blocks);
   }
 }
 
@@ -239,7 +251,7 @@ const questions = [
       }
     ],
     when(answers) {
-      return !answers.defaultFunctions
+      return !answers.defaultFunctions;
     }
   }
 ];
@@ -268,12 +280,13 @@ program
     const createBlock = new makeBlock({
       blockName: blocks,
       typeBlock: commands.type || 'block',
-      createWithDir: commands.folder
-    })
+      createWithDir: commands.folder,
+      defaultFunctions: true
+    });
 
     createBlock.create()
       .catch(err => {
-        console.log(err)
+        console.log(err);
       });
   })
   .parse(process.argv);
