bb7b6551c9f8a3e437ec341cf4c5f91047f59acf
Nikita Nafranets <eddimensi@gmail.com>
2017-07-26 12:14:57 +0300

    fix bugs, new add functional for create methods

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 const inquirer = require('inquirer');
 const program = require('commander');
 const changeCase = require('change-case');
@@ -8,9 +10,127 @@ const fsStat = promisify(fs.stat);
 const fsMkDir = promisify(fs.mkdir);
 const fsWriteFile = promisify(fs.writeFile);
 
+const folders = {
+  page: path.join(__dirname, 'src/pages'),
+  block: path.join(__dirname, 'src/components'),
+  element: path.join(__dirname, 'src/elements')
+};
+
+const fileSources = {
+  vue: blockName => `<template>
+  <div class="${changeCase.paramCase(blockName)}"></div>
+</template>
+
+<script>
+const props = [];
+
+const data = function () {
+  return {
+
+  };
+};
+
+const computed = {};
+
+const methods = {};
+
+const created = function () {};
+
+const mounted = function () {};
+
+const components = {};
+
+export default {
+  name: '${changeCase.paramCase(blockName)}',
+  props,
+  data,
+  computed,
+  methods,
+  created,
+  mounted,
+  components
+};
+</script>
+
+<style lang="scss">
+.${changeCase.paramCase(blockName)} {
+  display: block;
+}
+</style>
+
+`
+};
+
+
+
+class makeBlock {
+  constructor(options) {
+    this.options = options;
+  }
+
+  async directoryOrFileExist(blockPath, blockName) {
+    try {
+      await fsStat(blockPath)
+      throw `Error >>> Данный файл или директория уже существует ${blockName || blockPath}`
+    } catch (err) {
+      return true;
+    }
+  }
+
+  async createDir(dirPath) {
+    try {
+      await fsMkDir(dirPath)
+      return true;
+    } catch (err) {
+      throw `Error >>> Данный файл уже существует ${dirPath}`
+    }
+  }
+
+  async createFiles(blockPath, blockName) {
+    const files = [];
+
+    Object.keys(fileSources).forEach(ext => {
+      const fileSource = fileSources[ext](blockName);
+      const filename = `${blockName}.${ext}`;
+      const filePath = path.join(blockPath, filename);
+
+      files.push(fsWriteFile(filePath, fileSource, 'utf-8'))
+    })
+
+    try {
+      return await Promise.all(files);
+    } catch (err) {
+      console.error(`Error >>> Данный файл уже существует`);
+      throw err;
+    }
+  }
+
+  async makeBlock(blockName) {
+    let blockPath = path.join(folders[this.options.typeBlock])
+    if (this.options.createWithDir) {
+      blockPath = path.join(folders[this.options.typeBlock], blockName)
+    }
+
+    if (this.options.createWithDir) {
+      await this.directoryOrFileExist(blockPath)
+      await this.createDir(blockPath)
+    }
+    await this.createFiles(blockPath, blockName)
+  }
+
+  create() {
+    const blocks = this.options.blockName
+
+    if (Array.isArray(blocks)) {
+      const files = blocks.map(name => this.makeBlock(name));
+      return Promise.all(files);
+    }
+
+    return this.makeBlock(blocks)
+  }
+}
+
 console.log('Vue component generator');
-
-
 
 const questions = [
   {
@@ -92,7 +212,9 @@ const questions = [
 
 async function createBlockWithInterface() {
   const data = await inquirer.prompt(questions);
-  console.log(data)
+  const createBlock = new makeBlock(data);
+
+  return await createBlock.create();
 }
 
 program
@@ -108,128 +230,20 @@ program
         return false;
       }
     }
-    console.log('env', blocks, commands.type, commands.folder)
+
+    const createBlock = new makeBlock({
+      blockName: blocks,
+      typeBlock: commands.type || 'block',
+      createWithDir: commands.folder
+    })
+
+    createBlock.create()
+      .catch(err => {
+        console.log(err)
+      });
   })
   .parse(process.argv);
 
 if (!program.args.length) {
   createBlockWithInterface();
-}
-
-const folders = {
-  page: path.join(__dirname, 'src/pages'),
-  block: path.join(__dirname, 'src/components'),
-  element: path.join(__dirname, 'src/elements')
-};
-
-const fileSources = {
-  vue: blockName => `<template>
-  <div class="${changeCase.paramCase(blockName)}"></div>
-</template>
-
-<script>
-const props = [];
-
-const data = function () {
-  return {
-
-  };
-};
-
-const computed = {};
-
-const methods = {};
-
-const created = function () {};
-
-const mounted = function () {};
-
-const components = {};
-
-export default {
-  name: '${changeCase.paramCase(blockName)}',
-  props,
-  data,
-  computed,
-  methods,
-  created,
-  mounted,
-  components
-};
-</script>
-
-<style lang="scss">
-.${changeCase.paramCase(blockName)} {
-  display: block;
-}
-</style>
-
-`
-};
-
-
-
-class makeBlock {
-  constructor(options) {
-    this.options = options;
-  }
-
-  async directoryOrFileExist(blockPath, blockName) {
-    try {
-      await fsStat(blockPath)
-      return true;
-    } catch (err) {
-      throw `Error >>> Данный файл или директория уже существует ${blockName || blockPath}`
-    }
-  }
-
-  async createDir(dirPath) {
-    try {
-      await fsMkDir(dirPath)
-      return true;
-    } catch (err) {
-      throw `Error >>> Данный файл уже существует ${dirPath}`
-    }
-  }
-
-  async createFiles(blockPath, blockName) {
-    const files = [];
-
-    Object.keys(fileSources).forEach(ext => {
-      const fileSource = fileSources[ext](blockName);
-      const filename = `${blockName}.${ext}`;
-      const filePath = path.join(blockPath, filename);
-
-      files.push(fsWriteFile(filePath, fileSource, 'utf-8'))
-    })
-
-    try {
-      return await Promise.all(files);
-    } catch (err) {
-      console.error(`Error >>> Данный файл уже существует`);
-      throw err;
-    }
-  }
-
-  async makeBlock(blockName) {
-    let blockPath = path.join(folders[this.options.typeBlock])
-    if (this.options.createWithDir) {
-      blockPath = path.join(folders[this.options.typeBlock], blockName)
-    }
-
-    await this.directoryOrFileExist(blockPath)
-    await this.createDir(blockPath)
-    await this.createFiles(blockPath, blockName)
-  }
-
-  create() {
-    const blocks = this.options.blockName
-
-    if (Array.from(blocks)) {
-      const files = blocks.map(name => this.makeBlock(name));
-      return Promise.all(files);
-    }
-
-    return this.makeBlock(blocks)
-  }
 }
